edd7ee2c3b6802766c37ed2937eea3d220f7d8bf
Richard Ortiz <ortizrichard2@gmail.com>
2021-04-05 21:54:33 -0400

    completed dictionary and deletd an unecessary semicolon from average

--- average.py
+++ average.py
@@ -2,7 +2,7 @@
 
 for i in range (4):
     temp = int(input("gimme a number: "))
-    total = total + temp;
+    total = total + temp
 
 average = total / 4
 print(average)
---
+++ dictionary.py
@@ -0,0 +1,10 @@
+﻿myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
+y =[]
+x =[]
+
+for k, v in myData.items():
+    print('key:',k,', value:', v)
+    y.append(k)
+    x.append(v)
+print('ALL KEYS:', y)
+print('ALL VALUES:',x)
