5e7c4d3b0c9c0ec958f4335c10c90fece92a8a4b
Richard Ortiz <ortizrichard2@gmail.com>
2021-04-12 15:22:09 -0400

    Added prof's notes for dictionary file

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,10 @@
 ﻿myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
+
+#filling these arrays with keys and values
 y =[]
 x =[]
 
+# iteration in parallel 
 for k, v in myData.items():
     print('key:',k,', value:', v)
     y.append(k)
